2ddc60765e533a640e44d12073a035118a1e9997
Rafael <Rafael.viana@iesb.edu.br>
2023-05-22 18:02:44 -0300

    Atualizando ProjFrontEndPI

--- components/Pagina.js
+++ components/Pagina.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Container, Navbar } from 'react-bootstrap'
+import { Container, Nav, Navbar, Offcanvas } from 'react-bootstrap'
 
 function Pagina() {
     return (
         <div>
 
             {[false].map((expand) => (
-                <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+                <Navbar key={expand} bg="primary" expand={expand} className="mb-3" variant="dark">
                     <Container fluid>
                         <Navbar.Toggle aria-controls={``} />
-                        <Navbar.Brand href="#" className=''>Navbar Offcanvas</Navbar.Brand>
-                        <Navbar.Offcanvas id={``} aria-labelledby={``} placement="start"></Navbar.Offcanvas>
+                        <h3 className="">POLÍTICA TRANSPARENTE</h3>
+                        <Navbar.Brand href="#" className=''>PORTAL DOS DEPUTADOS</Navbar.Brand>
+                        <Navbar.Brand href="#" className=''></Navbar.Brand>
+                        <Navbar.Brand href="#" className=''></Navbar.Brand>
+                        <Navbar.Brand href="#" className=''></Navbar.Brand>
+                        <Navbar.Brand href="#" className=''></Navbar.Brand>
+                        <Navbar.Brand href="#" className=''></Navbar.Brand>
+                        <Navbar.Offcanvas id={``} aria-labelledby={``} placement="start">
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-end flex-grow-1 pe-3">
+                                    <Nav.Link href="#action1">Notícias</Nav.Link>
+                                    <Nav.Link href="#action2">Deputados</Nav.Link>
+                                    <Nav.Link href="#action2">Gastos Parlamentares</Nav.Link>
+                                    <Nav.Link href="#action2">Votações</Nav.Link>
+                                </Nav>
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
                     </Container>
                 </Navbar>
             ))}
